2028a774140b9899bce64309572be553f4b95042
AkashRajpurohit <akashrajpurohit.009@gmail.com>
2023-01-22 13:12:24 +0530

    feat: plugins on neovim

---
+++ nvim/.config/nvim/after/plugin/neodev.rc.lua
@@ -0,0 +1,6 @@
+-- Setup neovim lua configuration
+require('neodev').setup()
+--
+-- nvim-cmp supports additional completion capabilities, so broadcast that to servers
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
---
+++ nvim/.config/nvim/lua/akash/maps.lua
@@ -0,0 +1,41 @@
+local keymap = vim.keymap
+
+-- Set <space> as the leader key
+-- See `:help mapleader`
+--  NOTE: Must happen before plugins are required (otherwise wrong leader will be used)
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+-- Keymaps for better default experience
+-- See `:help vim.keymap.set()`
+keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
+
+-- Remap for dealing with word wrap
+keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+
+-- Increment/decrement
+keymap.set('n', '+', '<C-a>')
+keymap.set('n', '-', '<C-x>')
+
+-- Select all
+keymap.set('n', '<C-a>', 'gg<S-v>G')
+
+-- New tab
+keymap.set('n', 'te', ':tabedit')
+
+-- Split window
+keymap.set('n', 'ss', ':split<Return><C-w>w')
+keymap.set('n', 'sv', ':vsplit<Return><C-w>w')
+
+-- Move window
+keymap.set('n', '<Space>', '<C-w>w')
+keymap.set('', 'sh', '<C-w>h')
+keymap.set('', 'sk', '<C-w>k')
+keymap.set('', 'sj', '<C-w>j')
+keymap.set('', 'sl', '<C-w>l')
+
+-- Diagnostic keymaps
+keymap.set('n', '[d', vim.diagnostic.goto_prev)
+keymap.set('n', ']d', vim.diagnostic.goto_next)
+keymap.set('n', '<leader>e', vim.diagnostic.open_float)
+keymap.set('n', '<leader>q', vim.diagnostic.setloclist)
