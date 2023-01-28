8e49df5f9f5b6f1bcd919cc879add7261c5544d6
AkashRajpurohit <akashrajpurohit.009@gmail.com>
2023-01-28 16:16:23 +0530

    feat: use tokyonight theme

--- nvim/.config/nvim/lua/akash/base.lua
+++ nvim/.config/nvim/lua/akash/base.lua
@@ -30,7 +30,7 @@ vim.wo.signcolumn = 'yes'
 
 -- Set colorscheme
 vim.o.termguicolors = true
-vim.cmd [[colorscheme onedark]]
+vim.cmd[[colorscheme tokyonight]]
 
 -- Set completeopt to have a better completion experience
 vim.o.completeopt = 'menuone,noselect'
