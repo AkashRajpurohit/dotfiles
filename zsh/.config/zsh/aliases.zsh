a7fa37ac8333c704ba20ec87749100c76ea53120
AkashRajpurohit <me@akashrajpurohit.com>
2023-11-04 09:06:09 +0530

    feat: :sparkles: added new aliases and updated exports

--- zsh/.config/zsh/aliases.zsh
+++ zsh/.config/zsh/aliases.zsh
@@ -1,10 +1,10 @@
 #!/bin/sh
 alias j='z'
 alias f='zi'
-alias g='lazygit'
 alias zsh-update-plugins="find "$ZDOTDIR/plugins" -type d -exec test -e '{}/.git' ';' -print0 | xargs -I {} -0 git -C {} pull -q"
 alias nvimrc='nvim ~/.config/nvim/'
-alias yay="paru"
+alias mkcd='mkdir -p && cd' # make directory and cd into it
+alias fuck='sudo !!' # run the previous command with sudo
 
 # Changing "ls" to "exa"
 alias ls='exa -al --color=always --group-directories-first' # my preferred listing
@@ -26,6 +26,7 @@ alias rm='rm -i'
 # easier to read disk
 alias df='df -h'     # human-readable sizes
 alias free='free -m' # show sizes in MB
+alias duf='du -h d1' # show file sizes in a folder for depth=1
 
 # get top process eating memory
 alias psmem='ps auxf | sort -nr -k 4 | head -5'
@@ -41,6 +42,40 @@ alias gpg-check='gpg2 --keyserver-options auto-key-retrieve --verify'
 alias gpg-retrieve='gpg2 --keyserver-options auto-key-retrieve --receive-keys'
 
 # systemd
+manage_service() {
+  local action="$1"
+  local service="$2"
+
+  case "$action" in
+    restart)
+      sudo systemctl restart "$service"
+      ;;
+    status)
+      sudo systemctl status "$service"
+      ;;
+    enable)
+      sudo systemctl enable "$service"
+      ;;
+    disable)
+      sudo systemctl disable "$service"
+      ;;
+    enable-now)
+      sudo systemctl enable --now "$service"
+      ;;
+    *)
+      echo "Usage: manage-service {restart|status|enable|disable|enable-now} <service>"
+      return 1
+      ;;
+  esac
+}
+
+# Dynamic aliases for managing services
+alias restart_service="manage_service restart"
+alias status_service="manage_service status"
+alias enable_service="manage_service enable"
+alias disable_service="manage_service disable"
+alias enable_now_service="manage_service enable --now"
+
 alias running_services='systemctl list-units --type=service --state=running'
 
 # Replace cat with bat
--- zsh/.config/zsh/exports.zsh
+++ zsh/.config/zsh/exports.zsh
@@ -3,10 +3,6 @@
 HISTSIZE=1000000
 SAVEHIST=1000000
 export GPG_TTY=$(tty)
-export EDITOR="vim"
-export TERMINAL="kitty"
-export BROWSER="brave"
-export MANPAGER='vim +Man!'
 export MANWIDTH=999
 export GOPATH=$HOME/.local/share/go
 export BUN_INSTALL=$HOME/.bun
@@ -17,7 +13,6 @@ export PATH=$HOME/.local/share/go/bin:$PATH
 export PATH=$HOME/.local/share/fnm:$PATH
 export PATH=$HOME/.local/share/pnpm:$PATH
 export PATH=$HOME/.local/share/neovim/bin:$PATH
-export XDG_CURRENT_DESKTOP="Wayland"
 export FZF_CTRL_T_OPTS="--preview 'bat --color=always --line-range :500 {}'"
 
 eval "$(fnm env --use-on-cd)"
