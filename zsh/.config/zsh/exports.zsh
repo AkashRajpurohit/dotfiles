cc5a00184e861f2874875924239145ea6e12cb10
Akash Rajpurohit <me@akashrajpurohit.com>
2024-06-30 17:11:07 +0530

    chore: :recycle: remove cargo path from PATH

--- zsh/.config/zsh/exports.zsh
+++ zsh/.config/zsh/exports.zsh
@@ -8,7 +8,6 @@ export MANWIDTH=999
 export BUN_INSTALL=$HOME/.bun
 export PATH=$BUN_INSTALL/bin:$PATH
 export PATH=$HOME/.local/bin:$PATH
-export PATH=$HOME/.cargo/bin:$PATH
 export PATH=$HOME/.local/share/go/bin:$PATH
 export PATH=$HOME/.local/share/fnm:$PATH
 export PATH=$HOME/.local/share/pnpm:$PATH
